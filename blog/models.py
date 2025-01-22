7c7f33d7c02d5f280a36704b50c96f88dd1ec8e7
Rand Jalo <06w3oxsr@students.codeinstitute.net>
2025-01-22 18:52:34 +0000

    Add migrations

--- blog/models.py
+++ blog/models.py
@@ -13,3 +13,5 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     STATUS = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
